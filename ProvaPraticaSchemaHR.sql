871b06bcca9a6446c365954b0fd5c47837b7246e
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-08-07 16:29:28 -0300

    Resolvendo as questoes Nº1 da prova

--- ProvaPraticaSchemaHR.sql
+++ ProvaPraticaSchemaHR.sql
@@ -3,7 +3,10 @@
 
 
 --SCHEMA HR:
---1.Trazer a DATA da ULTIMA CONTRATACAO por DEPARTAMENTO:
+1. Gerar uma consulta para cada situação a baixo:
+
+
+A.Trazer a DATA da ULTIMA CONTRATACAO por DEPARTAMENTO:
 -- Cardinalidade N:N
 
 SELECT MAX(E.HIRE_DATE)AS ULTIMA_CONTRATACAO, 
@@ -15,7 +18,7 @@ GROUP BY D.DEPARTMENT_NAME;
 --------------------------------------------------------------------------------------------------
 --SCHEMA HR:
 
-2.Trazer todos os DEPARTAMENTOS que não são COMMISIONADO:
+B.Trazer todos os DEPARTAMENTOS que não são COMMISIONADOS:
 
 SELECT NVL(E.COMMISSION_PCT,0)AS COMMISAL_ZERO, 
            D.DEPARTMENT_NAME --,E.*, D.*
@@ -27,7 +30,7 @@ GROUP BY E.COMMISSION_PCT, D.DEPARTMENT_NAME;
 --------------------------------------------------------------------------------------------------
 --SCHEMA HR:
 
-3.Trazer todos os FUNCIONARIOS que tiveram no mínimo 2 PROMOCAO.
+C.Trazer todos os FUNCIONARIOS que tiveram no mínimo 2 PROMOCAO.
 
 SELECT DISTINCT(E.FIRST_NAME) AS NOME,
        COUNT(J.EMPLOYEE_ID)AS PROMOCOES     
@@ -40,6 +43,42 @@ HAVING COUNT(J.EMPLOYEE_ID) >= 2;
 --------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------
 --SCHEMA HR:
+
+D.Trazer a quantidade de FUNCIONARIOS demitidos por DEPARTAMENTO.
+
+SELECT COUNT(*) AS FIRED,
+       D.DEPARTMENT_NAME
+FROM EMPLOYEES E
+INNER JOIN DEPARTMENTS D
+ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
+WHERE DEMISSAL_DATE IS NOT NULL
+GROUP BY D.DEPARTMENT_NAME;
+
+--------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------
+--SCHEMA HR:
+
+E.Trazer o maior e menor SALARIO do funcionário agrupado por DEPARTAMENTO de SALES
+
+SELECT MIN(E.SALARY)AS MENOR_SALARIO,
+       MAX(E.SALARY)AS MAIOR_SALARIO,
+       D.DEPARTMENT_NAME AS DEPARTAMENTO
+FROM EMPLOYEES E
+INNER JOIN DEPARTMENTS D
+ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
+WHERE UPPER(D.DEPARTMENT_NAME) = 'SALES'
+GROUP BY D.DEPARTMENT_NAME;
+
+--------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------
+--SCHEMA HR:
+
+
+
+
+
+
+
 
 
 
